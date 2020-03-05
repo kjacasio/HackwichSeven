494a37e4a7622868bcbb44e96192abb55074c226
kat <kjacasio@hawaii.edu>
2020-03-05 10:20:10 -1000

    Parts 1-5 completed Mar 5

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //set display label to have no text
+        
+        self.displayLabel.text = ""
+        
     }
-
+    @IBAction func ButtonPressed(_ sender: Any)
+    {
+        
+        let userInputText = textField.text
+        
+        //display user text in displaylabel
+        
+        self.displayLabel.text = userInputText
+        
+    }
 
 }
 
